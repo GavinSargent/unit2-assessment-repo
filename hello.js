742cbcc1328bcc2214b563ce5c2cec0ecbda729c
Gavin Sargent <gavin.sargent3535@gmail.com>
2023-02-24 16:31:27 -0700

    added extra credit link to hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
 console.log(`You're ready to begin!`)
+
+//https://docs.google.com/document/d/1CbvAQTfKbGyx42Yn7ecWb1IUKRMBrNgh1Mn9_TDArzw/edit?usp=sharing
+//
+//This is the link to my extra credit
